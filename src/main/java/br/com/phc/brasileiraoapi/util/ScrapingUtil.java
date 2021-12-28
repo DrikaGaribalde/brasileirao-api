cc86f0bd77d4ac28d5b8cf67d1b92135c8a0fdb3
Adriana Garibalde <adrianagaribalde@gmail.com>
2021-12-27 22:01:58 -0300

    Criação do dto com dados de partida, criação do enum de status

---
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -0,0 +1,12 @@
+package br.com.phc.brasileiraoapi.util;
+
+public class ScrapingUtil {
+
+	private static final String BASE_URL_GOOGLE = "httpx://www.google.com.br/serarch?q=";
+	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-br";
+	public static void main(String[] args) {
+		
+
+	}
+
+}
