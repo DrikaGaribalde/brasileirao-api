b71b279716bf755476a3fa9cc3e29abd6ff3b85f
Adriana Garibalde <adrianagaribalde@gmail.com>
2021-12-27 21:27:29 -0300

    Subindo versão inicial do projeto

---
+++ src/main/java/br/com/phc/brasileiraoapi/BrasileiraoApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.phc.brasileiraoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoApiApplication.class, args);
+		System.out.println("oi to aqui :)");
+	}
+
+}
