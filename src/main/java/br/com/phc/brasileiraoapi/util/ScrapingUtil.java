8d41c397aa4e10360ccd9b86db182e04884387ff
Adriana Garibalde <adrianagaribalde@gmail.com>
2021-12-30 19:04:27 -0300

    adicionando todos os status das partidas

--- src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -14,29 +14,64 @@ public class ScrapingUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
 	private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
+
 	public static void main(String[] args) {
-		
-		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+
+		// 30-12-2021-arouca-x-braga
+		// Segunda aba
+		String url = BASE_URL_GOOGLE + "30-12-2021-arouca-x-braga"
+				+ COMPLEMENTO_URL_GOOGLE;
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obtemInformacoesPartida(url);
 
 	}
-	
+
 	public PartidaGoogleDto obtemInformacoesPartida(String url) {
 		PartidaGoogleDto partida = new PartidaGoogleDto();
 		//
 		Document document = null;
-		
+
 		try {
 			document = Jsoup.connect(url).get();
-			//recuperar informações da página
+			// recuperar informações da página
 			String title = document.title();
 			LOGGER.info("Título da Página {}", title);
+			// TESTE
+			obtemStatusPartida(document);
+
 		} catch (IOException e) {
 			LOGGER.error("Erro ao tentar conectar no Google com Jsoup!!! {}", e.getMessage());
 		}
-		
+
 		return partida;
+	}
+
+	// pegando dados da página
+	public StatusPartida obtemStatusPartida(Document document) {
+		// situações
+		// Partida não iniciada
+		// Partida iniciada || jogo rolando || intervalo
+		// Partida encerrada
+		// Penalidades
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if (!isTempoPartida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+			if (tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENALTIS;
+			}
+			LOGGER.info(tempoPartida);
+		}
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		if (!isTempoPartida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+
+		LOGGER.info(statusPartida.toString());
+
+		return statusPartida;
+
 	}
 
 }
