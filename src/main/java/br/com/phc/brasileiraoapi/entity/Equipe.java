c111da807930f64be60709b8653823b7900ce846
Adriana Garibalde <adrianagaribalde@gmail.com>
2022-01-06 22:21:04 -0300

    acrescentando a classe de configuração e ajustes no código

--- src/main/java/br/com/phc/brasileiraoapi/entity/Equipe.java
+++ src/main/java/br/com/phc/brasileiraoapi/entity/Equipe.java
@@ -19,20 +19,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="equipe")
-public class Equipe implements Serializable{
-	
+@Table(name = "equipe")
+public class Equipe implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="equipe_id")
+	@Column(name = "equipe_id")
 	private Long id;
 	
-	@Column(name="nome_equipe")
+	@Column(name = "nome_equipe")
 	private String nomeEquipe;
 	
-	@Column(name="url_logo_equipe")
+	@Column(name = "url_logo_equipe")
 	private String urlLogoEquipe;
-
 }
